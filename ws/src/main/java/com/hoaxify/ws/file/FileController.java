79cfb06ce3639fa1191f444b41160221147bdfb6
ferhad266 <ferhad.musayev.00@bk.ru>
2020-09-24 15:22:15 +0400

    SAVE TO DATABASE

--- ws/src/main/java/com/hoaxify/ws/file/FileController.java
+++ ws/src/main/java/com/hoaxify/ws/file/FileController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
-import java.util.Map;
 
 @RestController
 public class FileController {
@@ -15,8 +13,7 @@ public class FileController {
     FileService fileService;
 
     @PostMapping("/api/1.0/hoax-attachments")
-    Map<String, String> saveHoaxAttachment(MultipartFile file) {
-        String fileName = fileService.saveHoaxAttachment(file);
-        return Collections.singletonMap("name", fileName);
+    FileAttachment saveHoaxAttachment(MultipartFile file) {
+        return fileService.saveHoaxAttachment(file);
     }
 }
--- ws/src/main/java/com/hoaxify/ws/file/FileService.java
+++ ws/src/main/java/com/hoaxify/ws/file/FileService.java
@@ -12,6 +12,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
+import java.util.Date;
 import java.util.UUID;
 
 @Service
@@ -21,10 +22,13 @@ public class FileService {
 
     Tika tika;
 
-    public FileService(AppConfiguration appConfiguration) {
+    FileAttachmentRepository fileAttachmentRepository;
+
+    public FileService(AppConfiguration appConfiguration, FileAttachmentRepository fileAttachmentRepository) {
         super();
         this.appConfiguration = appConfiguration;
         this.tika = new Tika();
+        this.fileAttachmentRepository = fileAttachmentRepository;
     }
 
     public String writeBase64EncodedStringToFile(String image) throws IOException {
@@ -58,7 +62,7 @@ public class FileService {
         return tika.detect(base64encoded);
     }
 
-    public String saveHoaxAttachment(MultipartFile multipartFile) {
+    public FileAttachment saveHoaxAttachment(MultipartFile multipartFile) {
         String fileName = generateRandomName();
         File target = new File(appConfiguration.getUploadPath() + "/" + fileName);
         try {
@@ -68,6 +72,9 @@ public class FileService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return fileName;
+        FileAttachment attachment = new FileAttachment();
+        attachment.setName(fileName);
+        attachment.setDate(new Date());
+        return fileAttachmentRepository.save(attachment);
     }
 }
