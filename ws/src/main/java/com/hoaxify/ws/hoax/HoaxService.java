b3c8e2a7d7b7bfe7f262acf4915050999666c70c
ferhad266 <ferhad.musayev.00@bk.ru>
2020-09-26 21:31:58 +0400

    Deleting Affiliated Plugins.

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxService.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxService.java
@@ -2,6 +2,7 @@ package com.hoaxify.ws.hoax;
 
 import com.hoaxify.ws.file.FileAttachment;
 import com.hoaxify.ws.file.FileAttachmentRepository;
+import com.hoaxify.ws.file.FileService;
 import com.hoaxify.ws.hoax.vm.HoaxSubmitVM;
 import com.hoaxify.ws.user.User;
 import com.hoaxify.ws.user.UserService;
@@ -24,11 +25,14 @@ public class HoaxService {
 
     FileAttachmentRepository fileAttachmentRepository;
 
-    public HoaxService(HoaxRepository hoaxRepository, UserService userService, FileAttachmentRepository fileAttachmentRepository) {
+    FileService fileService;
+
+    public HoaxService(HoaxRepository hoaxRepository, UserService userService, FileAttachmentRepository fileAttachmentRepository, FileService fileService) {
         super();
         this.hoaxRepository = hoaxRepository;
         this.userService = userService;
         this.fileAttachmentRepository = fileAttachmentRepository;
+        this.fileService = fileService;
     }
 
     public void save(HoaxSubmitVM hoaxSubmitVM, User user) {
@@ -101,6 +105,11 @@ public class HoaxService {
     }
 
     public void delete(long id) {
+        Hoax inDB = hoaxRepository.getOne(id);
+        if (inDB.getFileAttachment() != null) {
+            String fileName = inDB.getFileAttachment().getName();
+            fileService.deleteAttachmentFile(fileName);
+        }
         hoaxRepository.deleteById(id);
     }
 }
